1050241ccf60428016b244fa1f1e7d9251294ae3
Userciph <youssef.amr5793@gmail.com>
2023-06-29 16:03:53 -0700

    More pointers, arrays and strings.

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,4 +1,11 @@
-string_toupper(char *n)
+#include "main.h"
+/**
+ * string_toupper - change all lowercase to uppercase
+ * @n: pointer
+ *
+ * Return: n
+ */
+char *string_toupper(char *n)
 {
 	int i;
 
