e101f3e07ee6f4984600f67e4b142795520614ec
Userciph <youssef.amr5793@gmail.com>
2023-07-05 09:13:20 -0700

    0x08. C - Recursion

---
+++ 0x08-recursion/0-puts_recursion.c
@@ -0,0 +1,18 @@
+#include"main.h"
+/**
+ * _puts_recursion-function as puts();
+ * @s:input
+ * Ruturn:Always 0(sucess)
+ */
+void _puts_recursion(char *s)
+{
+	if(*s)
+	{
+		_putchar(*s);
+		_puts_recursion(s+1);
+	}
+	else
+	{
+		_putchar(\n)
+	}
+}
---
+++ 0x08-recursion/1-print_rev_recursion.c
@@ -0,0 +1,15 @@
+#include"stdio.h"
+/**
+ * _print_rev_recursion fuction that prints string in reverse
+ * @s: The string to be printed
+ *
+ * Return 0(success)
+ */
+void _print_rev_recursion(char *s)
+{
+	if(*s)
+	{
+		_print_rev_recursion(s+1);
+		_putchar(*s);
+	}
+}
